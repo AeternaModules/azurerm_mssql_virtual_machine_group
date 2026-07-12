042aa4cb1cd59dd9e0528b908b56e1f606675888
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "mssql_virtual_machine_groups" {
-  description = "All mssql_virtual_machine_group resources"
-  value       = azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups
-  sensitive   = true
-}
 output "mssql_virtual_machine_groups_location" {
-  description = "List of location values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.location]
+  description = "Map of location values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.location }
 }
 output "mssql_virtual_machine_groups_name" {
-  description = "List of name values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.name]
+  description = "Map of name values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.name }
 }
 output "mssql_virtual_machine_groups_resource_group_name" {
-  description = "List of resource_group_name values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.resource_group_name }
 }
 output "mssql_virtual_machine_groups_sql_image_offer" {
-  description = "List of sql_image_offer values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.sql_image_offer]
+  description = "Map of sql_image_offer values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.sql_image_offer }
 }
 output "mssql_virtual_machine_groups_sql_image_sku" {
-  description = "List of sql_image_sku values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.sql_image_sku]
+  description = "Map of sql_image_sku values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.sql_image_sku }
 }
 output "mssql_virtual_machine_groups_tags" {
-  description = "List of tags values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.tags]
+  description = "Map of tags values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.tags }
 }
 output "mssql_virtual_machine_groups_wsfc_domain_profile" {
-  description = "List of wsfc_domain_profile values across all mssql_virtual_machine_groups"
-  value       = [for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : v.wsfc_domain_profile]
+  description = "Map of wsfc_domain_profile values across all mssql_virtual_machine_groups, keyed the same as var.mssql_virtual_machine_groups"
+  value       = { for k, v in azurerm_mssql_virtual_machine_group.mssql_virtual_machine_groups : k => v.wsfc_domain_profile }
   sensitive   = true
 }
 
